4c3f6b587ac94bd712010373dd59764759e5f45f
mohannad.inairat <mohannad.inairat@iqvia.com>
2023-03-14 18:31:39 +0200

    tests and db service

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator Function
+"""
+
+def add(x, y):
+    """ Add x and y and return sum """
+    return x + y
+def subtract(x, y):
+    """ subtract x and y and return result """
+    return y - x
